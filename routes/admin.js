b38ba4bb329010e62d5696c4803fc75650879bef
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-10-05 12:38:11 +0530

    Adding the controllers

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,16 @@
 const path = require('path');
 
 const express = require('express');
+
 const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
+
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
