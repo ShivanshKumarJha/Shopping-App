43437269b0047b30c5712486c8baf7198edc3f1b
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-10-05 16:48:54 +0530

    Refactoring our code

--- models/product.js
+++ models/product.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const rootDir = require('../util/path');
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) cb([]);
+    else cb(JSON.parse(fileContent));
+  });
+};
 
 module.exports = class Product {
   constructor(t) {
@@ -8,12 +16,7 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
@@ -22,10 +25,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      if (err) cb([]);
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
