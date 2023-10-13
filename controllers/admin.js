e57a1af88e6fef3a6ca5013909d9a56791225635
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-10-13 10:51:38 +0530

    Storing product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,11 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -23,5 +27,5 @@ exports.getProducts = (req, res, next) => {
       pageTitle: 'Admin Products',
       path: '/admin/products',
     });
-  });   
+  });
 };
