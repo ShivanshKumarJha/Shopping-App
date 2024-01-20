c1e352cf7229a6879816a510ec094e61aee31db7
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-01-21 03:19:24 +0530

    added timestamps

--- models/order.js
+++ models/order.js
@@ -2,24 +2,27 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({
-  products: [
-    {
-      product: { type: Object, required: true },
-      quantity: { type: Number, required: true },
-    },
-  ],
-  user: {
-    name: {
-      type: String,
-      required: true,
-    },
-    userId: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
+const orderSchema = new Schema(
+  {
+    products: [
+      {
+        product: { type: Object, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+    user: {
+      name: {
+        type: String,
+        required: true,
+      },
+      userId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+      },
     },
   },
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model('Order', orderSchema);
