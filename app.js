c115e5a15686330f666805c0fdce072327a64b93
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-01-31 20:14:07 +0530

    final update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-require('dotenv').config();
+const env = require('dotenv').config();
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
